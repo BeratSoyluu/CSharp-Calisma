c3dbcf22c0e8a6f8d04e70edcb25dcbe16c41dc2
BeratSoyluu <berat.soyluuu@gmail.com>
2026-02-21 16:47:35 +0300

    Erişim Belirleyiviler eklendi.

---
+++ 010-Erisim_Belirleyiciler/Kullanici.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_Erisim_Belirleyiciler
+{
+    public class Kullanici
+    {
+        public string Adi; // Program class ında bu değişkene ulaşmak için string in önüne public ekledik, yoksa orada erişemiyorduk
+        internal string Soyadi; // string in önüne internal yazmak, böylece aynı projede olduğu için bu değişkene ulaşılabilir hale geliyor.
+        private string Telefon; // private yaptığımız için program class ında gelmez ama bu class da kullanabiliriz.
+        protected string Email; // program cs den erişemeyiz protected yaptığımız için, bu sınıfta veya buradan miras alan bir sınıfta kullanabiliriz.
+        string sifre;
+    }
+    internal class Adres
+    {
+        private class Siparis
+        {
+
+        }
+    }
+    // Ana sınıflar sadece public ve internal alabilir.
+    // Ana sınıf içerisinde private ve protected erişim belirteci alabilir sınflar.
+    // protected ı kullanamayız.
+    // private ı kullanamayız.
+    // Enumlar erişim belirteci almazlar çünkü her zaman public ler.
+}
--- 010-Erisim_Belirleyiciler/Program.cs
+++ 010-Erisim_Belirleyiciler/Program.cs
@@ -4,18 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+Public: Kod tüm sınıflar tarafından erişilebilir durumdadır.
+Private: Kod yalnızca aynı sınıf içinde erişilebilir.
+Protected: Kod, aynı sınıf içinde veya o sınıftan miras alan bir sınıfta erişilebilir durumdadır.
+İnternal: Kod yalnızca kendi derlemesi içinde erişilebilir, başka bir derlemeden erişilemez.
+*/
 namespace _010_Erisim_Belirleyiciler
 {
     internal class Program
     {
+        public class deneme
+        {
+            public string UrunAdi = "Public öğeye herkes erişebilir";
+            private string urunAdi;
+            public void Toplam()
+            {
+
+            }
+
+            protected bool MailGonder()
+            {
+                return true;
+            }
+        }
+
+        protected class test // Ait olduğu sınıftan veya o sınıftan türetilen sınıflardan erişilebilir. (Burada Program class ının içindekiler erişebilir veya Program claasından türetilen sınıflar erişebilir.)
+        {
+            private string UrunAdi;
+        }
+
+        internal class urun // internal erişim belirteci etkin projeye ait sınıflardan erişilebilir, onların dışında erişilemez.   (Etkin proje: Çalıştırdığımız proje)
+        {
+            private string gizli = "Private sadece bulunduğu sınıftan erişilebilir, dıştaki sınıflardan erişilemez. (gizli değişkenimiz sadece urun class'ından erişilebilir.)";
+        }
         static void Main(string[] args)
         {
-            /*
-            Public: Kod tüm sınıflar tarafından erişilebilir durumdadır.
-            Private: Kod yalnızca aynı sınıf içinde erişilebilir.
-            Protected: Kod, aynı sınıf içinde veya o sınıftan miras alan bir sınıfta erişilebilir durumdadır.
-            İnternal: Kod yalnızca kendi derlemesi içinde erişilebilir, başka bir derlemeden erişilemez.
-             */
+            Kullanici kullanici = new Kullanici();
+            kullanici.Adi = "Ali";
+            kullanici.Soyadi = "Cambaz";
+
+            Console.WriteLine("Kullanıcı Adı: " + kullanici.Adi);
+            Console.WriteLine("Kullanıcı Soyadı: " + kullanici.Soyadi);
+
         }
     }
 }
