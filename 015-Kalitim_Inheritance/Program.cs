a2d7879a3fa48af69a0cc2686e32c4a3d33994dc
BeratSoyluu <berat.soyluuu@gmail.com>
2026-02-25 16:06:52 +0300

    Kalıtım çalışıldı.

---
+++ 015-Kalitim_Inheritance/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015_Kalitim_Inheritance
+{
+    class AtaSinif
+    {
+        public AtaSinif()
+        {
+            Console.WriteLine("AtaSinif kurucu metodu çalıştı");
+        }
+        public void Yaz()
+        {
+            Console.WriteLine("AtaSinif içindeki Yaz metodu çalıştı");
+        }
+    }
+
+    class OgulSinif : AtaSinif // Miras böyle alınır.
+    {
+        public OgulSinif()
+        {
+            Console.WriteLine("OgulSinif kurucu metodu çalıştı");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OgulSinif ogul = new OgulSinif();
+            ogul.Yaz();
+            // Çıktısı:
+            //  AtaSinif kurucu metodu çalıştı
+            //  OgulSinif kurucu metodu çalıştı
+            //  AtaSinif içindeki Yaz metodu çalıştı
+
+            Console.WriteLine("**********************************************************************************************************");
+            ApartmanKati apartmanKati = new ApartmanKati();
+            apartmanKati.EvSahibiYaz("Müfit Bey");
+            apartmanKati.sahip = "Müfit Bey";
+            apartmanKati.EvSahibiYaz(apartmanKati.sahip);
+        }
+    }
+    class Ev
+    {
+        public string sahip;
+        public void EvSahibiYaz(string sahip)
+        {
+            Console.WriteLine("Ev Sahibi: " + sahip);
+        }
+    }
+    class ApartmanKati : Ev
+    {
+        internal string apartmanYoneticisi;
+        public void ApartmanYoneticisiniYaz(string apartmanYoneticisi)
+        {
+            Console.WriteLine("Apartman Yönetici: " + apartmanYoneticisi);
+        }
+    }
+}
